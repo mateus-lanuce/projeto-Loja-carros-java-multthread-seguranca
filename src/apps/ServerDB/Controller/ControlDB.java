5889d6026aea123bbbba634bcf9e2b3d5c32303c
Valentina Lacerda <valentinaslacerda07@gmail.com>
2024-04-09 16:14:53 -0300

    feat: métodos de remoção, alteração, listagens e buscas

--- src/apps/ServerDB/Controller/ControlDB.java
+++ src/apps/ServerDB/Controller/ControlDB.java
@@ -30,7 +30,6 @@ public class ControlDB extends UnicastRemoteObject implements CarrosInterface {
 
     /**
      * funcao de validação da conexão das replicas
-     * modifica o array de replicas para conter apenas as replicas conectadas
      */
     private void validateReplicas() {
         replicDBsConnected.clear();
@@ -94,12 +93,133 @@ public class ControlDB extends UnicastRemoteObject implements CarrosInterface {
         return carroLocal;
     }
 
+    public LinkedList<Carro> sync_removerPorNome(String nome) throws RemoteException{
+        //remove o carro na replica local
+        LinkedList<Carro> carros = carrosHashMap.removerPorNome(nome);
+
+        validateReplicas();
+        Thread t = new Thread(() -> {
+            try {
+                //TODO: rever a logica para o caso de falha na primeira replica
+                replicDBsConnected.get(0).removerPorNome(nome);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        });
+
+        t.start();
+
+        //aguarda a primeira replica salvar o dado
+        try {
+            t.join();
+
+            //como a primeira replica salvou, podemos enviar para as demais assincronamente enquanto retornamos o dado
+            replicDBsConnected.stream().skip(1).forEach(replica -> {
+                Thread t2 = new Thread(() -> {
+                    try {
+                        replica.removerPorNome(nome);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                });
+                t2.start();
+            });
+
+            return carros;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return carros;
+    }
+    public Carro sync_remover(String renavam) throws IllegalArgumentException, RemoteException{
+        //remove o carro na replica local
+        Carro carroLocal = carrosHashMap.remover(renavam);
+
+        validateReplicas();
+        Thread t = new Thread(() -> {
+            try {
+                //TODO: rever a logica para o caso de falha na primeira replica
+                replicDBsConnected.get(0).remover(renavam);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        });
+
+        t.start();
+
+        //aguarda a primeira replica salvar o dado
+        try {
+            t.join();
+
+            //como a primeira replica salvou, podemos enviar para as demais assincronamente enquanto retornamos o dado
+            replicDBsConnected.stream().skip(1).forEach(replica -> {
+                Thread t2 = new Thread(() -> {
+                    try {
+                        replica.remover(renavam);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                });
+                t2.start();
+            });
+
+            return carroLocal;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return carroLocal;
+    }
+
+    public Carro sync_alterar(String renavam, Carro carro) throws IllegalArgumentException, RemoteException{
+        //adiciona o carro na replica local
+        Carro carroLocal = carrosHashMap.alterar(renavam, carro);
+
+        // valida as replicas conectadas
+        validateReplicas();
+
+        // adicionar na primeira replica
+        Thread t = new Thread(() -> {
+            try {
+                //TODO: rever a logica para o caso de falha na primeira replica
+                replicDBsConnected.get(0).alterar(renavam, carro);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        });
+
+        t.start();
+
+        //aguarda a primeira replica salvar o dado
+        try {
+            t.join();
+
+            //como a primeira replica salvou, podemos enviar para as demais assincronamente enquanto retornamos o dado
+            replicDBsConnected.stream().skip(1).forEach(replica -> {
+                Thread t2 = new Thread(() -> {
+                    try {
+                        replica.alterar(renavam, carro);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                });
+                t2.start();
+            });
+
+            return carroLocal;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return carroLocal;
+    }
+
     @Override
     public Carro adicionar(Carro carro) throws IllegalArgumentException, RemoteException {
         //a sincronizacao deve aguardar que o dado tenha sido salvo em uma replica antes de retornar
         return sync_adicionar(carro);
     }
-
     @Override
     public Carro remover(String renavam) throws IllegalArgumentException, RemoteException {
         return null;
@@ -110,19 +230,22 @@ public class ControlDB extends UnicastRemoteObject implements CarrosInterface {
         return null;
     }
 
+
+
     @Override
     public LinkedList<Carro> getCarros(Categoria categoria) throws RemoteException {
-        return null;
+
+        return carrosHashMap.getCarros(categoria);
     }
 
     @Override
     public LinkedList<Carro> getCarrosByNome(String nome) throws RemoteException {
-        return null;
+        return carrosHashMap.getCarrosByNome(nome);
     }
 
     @Override
     public Carro getCarroByRenavam(String renavam) throws RemoteException {
-        return null;
+        return carrosHashMap.getCarroByRenavam(renavam);
     }
 
     @Override
@@ -132,7 +255,7 @@ public class ControlDB extends UnicastRemoteObject implements CarrosInterface {
 
     @Override
     public int getQuantidade() throws RemoteException {
-        return 0;
+        return carrosHashMap.getQuantidade();
     }
 
     @Override
