b71e8437f8ad3d547dcce2a3b3f9dc3d8e15ecab
Valentina Lacerda <valentinaslacerda07@gmail.com>
2024-04-13 15:16:17 -0300

    feat: ServerLoja

--- src/apps/ServerLoja/ServerLoja.java
+++ src/apps/ServerLoja/ServerLoja.java
@@ -1,23 +1,33 @@
 package apps.ServerLoja;
 
+import apps.Records.IpPort;
 import apps.ServerDB.Controller.ControlDB;
 import apps.ServerDB.ServerDB;
+import apps.ServerLoja.Controller.ControlLoja;
 
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.ArrayList;
 
 public class ServerLoja {
     public ServerLoja() {
         try {
+            IpPort port1 = new IpPort("", 1099);
+            IpPort port2 = new IpPort("", 1100);
+            IpPort port3 = new IpPort("", 1101);
+            ArrayList<IpPort> ports = new ArrayList<>();
+            ports.add(port1);
+            ports.add(port2);
+            ports.add(port3);
 
             //cria o objeto remoto
-            ControlDB carros = new ControlDB();
+            ControlLoja lojas = new ControlLoja(ports, 1);
 
-            Registry registry = LocateRegistry.createRegistry();
+            Registry registry = LocateRegistry.createRegistry(1099);
 
             //registra o objeto remoto
-            registry.rebind("Carros", carros);
-            System.out.println("Servidor de banco de dados pronto");
+            registry.rebind("Lojas", lojas);
+            System.out.println("Servidor de loja pronto");
 
         } catch (Exception e) {
             e.printStackTrace();
